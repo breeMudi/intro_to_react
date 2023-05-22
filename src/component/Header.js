1e62bbee6a5c1ecf6ed201de6626679a9224d271
Bridget Mudiaga-Ojemu <65234249+breeMudi@users.noreply.github.com>
2023-05-22 22:46:10 +0200

    Update Header.js
    map navigation bar

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export default function Header() {
+  const items = [
+    'Home', 'About', 'Contact Us', 'Feedback'
+  ]
   return (
     <div>
       <p>This is my first self done header</p>
+      <ul className='navigation'>{items.map((item) => {return <li key={item}>{item}</li>})}</ul>
     </div>
   )
 }
