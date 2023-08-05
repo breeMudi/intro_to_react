5bdefd37f46822805f779ca86bd58c1b4c7b519a
Bridget Mudiaga-Ojemu <65234249+breeMudi@users.noreply.github.com>
2023-08-05 23:24:45 +0200

    Update Increament.js
    add use effect

--- src/component/Increament.js
+++ src/component/Increament.js
@@ -1,11 +1,11 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import React from 'react'
 
 function Increament() {
     const [count, setNew] = useState(0)
   return (
     <div>
-      <p>value: {count}</p>
+      <p>value: {`the current count is: ${count}`}</p>
       <button className="btn" onClick={() => setNew(count + 1)}>Increase</button>
       <button className="btn" onClick={() => setNew(count - 1)}>Reduce</button>
     </div>
